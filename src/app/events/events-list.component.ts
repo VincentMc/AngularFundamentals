08996e9c877bd76b612540f271dd8732e48e04a3
VincentMC <vincemcgaley@gmail.com>
2018-07-16 22:14:59 +0100

    Communicating with Child and Parent components

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -3,11 +3,20 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'events-list',
-    templateUrl: './events-list.component.html' /* Using a URL that points to a HTML file is better when the
+    template: `
+        <div>
+            <h1>Upcoming Angular Events</h1>
+            <hr/>
+            <event-thumbnail [event]="event1">
+            </event-thumbnail>
+        </div>
+    `
+    // When we add [] to our html tag it means that it expects an object to be passed to it
+    /* Using a URL that points to a HTML file is better when the
                                                 amount of HTML being put into the template gets too large */
 })
 export class EventsListComponent {
-    event = {
+    event1 = {
         id: 1,
         name: 'Vicents Angular Event',
         date: '09/26/2018',
